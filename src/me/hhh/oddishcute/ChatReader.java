37ec40e4501cab2c136cd441755157edce2b4b15
BuildTools <unconfigured@null.spigotmc.org>
2022-11-15 16:07:11 +0100

    The huhu update

--- src/me/hhh/oddishcute/ChatReader.java
+++ src/me/hhh/oddishcute/ChatReader.java
@@ -7,31 +7,41 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Locale;
 
 public class ChatReader implements Listener
 {
   @EventHandler
-  public void onPlayerChat(AsyncPlayerChatEvent event){
+  public void onPlayerChat(AsyncPlayerChatEvent event)
+  {
     String message = event.getMessage();
 
     Player player = event.getPlayer();
+    ArrayList<Player> players = (ArrayList<Player>) Bukkit.getServer().getOnlinePlayers();
 
-    if(player.getName().equals("LowerCaseH"))
+    if (player.getName().equals("LowerCaseH"))
     {
       return;
     }
 
-    if(message.toLowerCase().contains("aha") && !message.toLowerCase().contains("hah"))
+    if (message.toLowerCase().contains("huhu"))
     {
-      if(player.getName().equals("uwu_Meowth"))
+      for (int i = 0; i < players.size(); i++)
+      {
+        players.get(i).sendMessage(ChatColor.GREEN + "Huhu" + player.getName());
+      }
+    }
+    if (message.toLowerCase().contains("aha") && !message.toLowerCase().contains("hah"))
+    {
+      if (player.getName().equals("uwu_Meowth"))
       {
         event.setCancelled(true);
-        player.sendMessage(ChatColor.RED+"Schnauze du Versager");
+        player.sendMessage(ChatColor.RED + "Schnauze du Versager");
         return;
       }
     }
-
 
 
     Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.instance, new Runnable()
@@ -39,27 +49,28 @@ public class ChatReader implements Listener
       @Override
       public void run()
       {
-        if(message.toLowerCase().contains("aha") && !message.toLowerCase().contains("hah"))
+        if (message.toLowerCase().contains("aha") && !message.toLowerCase().contains("hah"))
         {
-          if(message.toLowerCase().contains("und jetzt"))
+          if (message.toLowerCase().contains("und jetzt"))
           {
-            player.sendMessage(ChatColor.RED+"...wird deine ganze Familie umgebracht "+player.getName()+", hahahaha");
+            player.sendMessage(ChatColor.RED + "...wird deine ganze Familie umgebracht " + player.getName() + ", hahahaha");
           }
-          else{
-            player.sendMessage(ChatColor.RED+"Jetzt wird deine ganze Familie umgebracht "+player.getName()+", hahahaha");
+          else
+          {
+            player.sendMessage(ChatColor.RED + "Jetzt wird deine ganze Familie umgebracht " + player.getName() + ", hahahaha");
           }
         }
-        else if(message.toLowerCase().contains("junge"))
+        else if (message.toLowerCase().contains("junge"))
         {
           player.sendMessage(ChatColor.DARK_PURPLE + "seethe and cope");
         }
-        else if(message.toLowerCase().contains("schön für dich " +player.getName()))
+        else if (message.toLowerCase().contains("schön für dich " + player.getName()))
         {
           player.sendMessage(ChatColor.DARK_PURPLE + "Schöner als du allemal du Hackfresse");
         }
-        else if(message.toLowerCase().contains("und jetzt"))
+        else if (message.toLowerCase().contains("und jetzt"))
         {
-          player.sendMessage(ChatColor.RED+"...wird deine ganze Familie umgebracht "+player.getName()+", hahahaha");
+          player.sendMessage(ChatColor.RED + "...wird deine ganze Familie umgebracht " + player.getName() + ", hahahaha");
         }
       }
     }, 10L);
